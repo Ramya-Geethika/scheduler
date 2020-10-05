1b688a1eeba5068471d6c15ea6884d6c9ca120ad
Ramya-Geethika <geethikakramya@gmail.com>
2020-10-05 19:42:28 -0400

    Components added

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,53 @@
 import React from "react";
-
+import DayList from 'components/DayList.js';
+import Appointment from "components/Appointment";
 import "components/Application.scss";
+import { getAppointmentsForDay, getInterviewersForDay } from "../helpers/selectors";
+import { getInterview } from "../helpers/selectors";
+import useApplicationData from '../hooks/useApplicationData';
 
 export default function Application(props) {
+  const { state, bookInterview, cancelInterview, setDay } = useApplicationData();
+
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"></nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>``
-        
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {getAppointmentsForDay(state, state.day).map((appointment) => {
+          const interview = getInterview(state, appointment.interview);
+          const interviewers = getInterviewersForDay(state, state.day);
+
+          return <Appointment
+            key={appointment.id}
+            {...appointment}
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview}
+            interviewers={interviewers}
+            bookInterview={bookInterview}
+            cancelInterview={cancelInterview}
+          />
+        })}
+        <Appointment key="last" time="18pm" />
       </section>
     </main>
   );
